63408c64bcb96e110c2d28a4871df6f6ab2081fd
Natallia Panfilenka <choco-cat@mail.ru>
2021-01-11 20:15:53 +0300

    feat: set API to RESTful

--- server.js
+++ server.js
@@ -26,31 +26,63 @@ app.get(`${URL}/users`, (request, response) => {
 });
 
 // endpoint to check user login
-app.post(`${URL}//users/login`, (request, response) => {
+app.post(`${URL}/users/login`, (request, response) => {
   db.all('SELECT * FROM users WHERE email = ? AND password = ?', request.body.email, request.body.password,
     (error, rows) => response.send(JSON.stringify(rows)));
 });
 
-// endpoint to get all users in the database
+// endpoint to get some user from the database
 app.get(`${URL}/users/:id`, (request, response) => {
   db.all('SELECT * from users WHERE id = (?)', request.params.id, (err, rows) => response.send(JSON.stringify(rows)));
 });
 
-// endpoint to add a user to the database
-app.post(`${URL}/users/add`, (request, response) => {
+// add a user to the database
+app.post(`${URL}/users`, (request, response) => {
   const values = [];
   const fields = Object.keys(request.body);
   fields.forEach((field) => values.push(request.body[field]));
   if (!process.env.DISALLOW_WRITE) {
     db.run(`INSERT INTO users (${fields.join(',')}) VALUES ("${values.join('","')}")`, (error) => {
       if (error) {
-        response.send({ message: `INSERT INTO users (${fields.join(',')}) VALUES ("${values.join('","')}")` });
+        response.send({ message: `Error INSERT new user` });
       } else {
         response.send({ message: 'success' });
       }
     });
   }
 });
+
+// update user in the database
+app.put(`${URL}/users/:id`, (request, response) => {
+  const fields = Object.keys(request.body);
+  const query = [];
+  fields.forEach((field) => {
+    query.push(`${field} = '${request.body[field]}'`);
+  });
+  db.run(`UPDATE users SET ${query.join(', ')} WHERE id = ${request.params.id}`, (error) => {
+    if (error) {
+      response.send({ message: `Error UPDATE user` });
+    } else {
+      //response.send({ message: 'success' });
+    }
+  });
+  db.all(`SELECT * FROM users WHERE id = ${request.params.id}`,
+    (error, rows) => response.send(JSON.stringify(rows)));
+});
+
+// delete a user from the database
+app.delete(`${URL}/users/:id`, (request, response) => {
+  if (!process.env.DISALLOW_WRITE) {
+    db.run(`DELETE FROM users WHERE id = ${request.params.id}`, (error) => {
+      if (error) {
+        response.send({ message: `Error DELETE user` });
+      } else {
+        response.send({ message: 'success' });
+      }
+    });
+  }
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
